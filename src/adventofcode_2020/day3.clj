5bbf796dba907e708ed3a68874bfaa12745d5bb6
Leandro López (inkel) <inkel.ar@gmail.com>
2020-12-07 15:56:16 -0300

    Add day 3 part 2 solution

--- src/adventofcode_2020/day3.clj
+++ src/adventofcode_2020/day3.clj
@@ -4,27 +4,31 @@
 (require '[clojure.java.io :as io]
          '[clojure.string :as str])
 
-(defn next-pos [x y width height]
-  (let [x (+ x 3)
-        y (inc y)]
-    (if (> y height)
-      nil
-      (if (> x width)
-        (list (- x width) y)
-        (list x y)))))
+(defn next-pos
+  ([x y width height] (next-pos x y 3 1 width height))
+  ([x y dx dy width height]
+   (let [x (+ x dx)
+         y (+ y dy)]
+     (if (> y height)
+       nil
+       (if (> x width)
+         (list (- x width) y)
+         (list x y))))))
 
-(defn count-trees [input]
-  (let [width (count (first input))
-        height (count input)]
-    (loop [x 1
-           y 1
-           acc 0]
-      (let [[x y] (next-pos x y width height)]
-        (if (nil? x)
-          acc
-          (let [row (get input (dec y))
-                c (get row (dec x))]
-            (recur x y (+ acc (if (= c \#) 1 0)))))))))
+(defn count-trees
+  ([input] (count-trees input 3 1))
+  ([input dx dy]
+   (let [width (count (first input))
+         height (count input)]
+     (loop [x 1
+            y 1
+            acc 0]
+       (let [[x y] (next-pos x y dx dy width height)]
+         (if (nil? x)
+           acc
+           (let [row (get input (dec y))
+                 c (get row (dec x))]
+             (recur x y (+ acc (if (= c \#) 1 0))))))))))
 
 (defn- input []
   (->> "day3.txt"
@@ -34,3 +38,12 @@
 
 (defn part1 []
   (count-trees (input)))
+
+(def slopes [[1 1] [3 1] [5 1] [7 1] [1 2]])
+
+(defn count-trees-by-slopes [input slopes]
+  (for [[dx dy] slopes]
+    (count-trees input dx dy)))
+
+(defn part2 []
+  (apply * (count-trees-by-slopes (input) slopes)))
--- test/adventofcode_2020/day3_test.clj
+++ test/adventofcode_2020/day3_test.clj
@@ -20,4 +20,12 @@
   (is (nil? (next-pos 4 2 10 2))))
 
 (deftest test-count-trees
-  (is (= 7 (count-trees input))))
+  (is (= 7 (count-trees input)))
+  (is (= 2 (count-trees input 1 1)))
+  (is (= 7 (count-trees input 3 1)))
+  (is (= 3 (count-trees input 5 1)))
+  (is (= 4 (count-trees input 7 1)))
+  (is (= 2 (count-trees input 1 2))))
+
+(deftest test-count-trees-by-slopes
+  (is (= [2 7 3 4 2] (count-trees-by-slopes input slopes))))
